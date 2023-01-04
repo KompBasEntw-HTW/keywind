530c560b7f5512b1b2b28082a0c2f3fa872976c0
Vincent Dlugosch <dlugoschvincent@gmail.com>
2023-01-04 04:47:51 +0100

    feat: add a little bit of customisation to keywind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.blue,
+        primary: colors.amber,
         secondary: colors.gray,
-
+        brand: {
+          DEFAULT: '#009687',
+          50: '#F1F9F8',
+          100: '#E2F3F1',
+          200: '#BFE5E1',
+          300: '#C2FFF9',
+          400: '#99FFF5',
+          500: '#00B8A5',
+          600: '#009687',
+          700: '##007A6E',
+          800: '#00665C',
+          900: '##005249',
+        },
         provider: {
           bitbucket: '#0052CC',
           facebook: '#1877F2',
